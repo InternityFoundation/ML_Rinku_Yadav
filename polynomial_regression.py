ed0400674d31cc5823435cc830eb6aa14ec21790
Rinku Yadav <42610036+rinkuyadav8955@users.noreply.github.com>
2019-01-05 08:15:30 +0530

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -14,16 +14,6 @@ y = dataset.iloc[:, 2].values
 """from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)"""
-
-# Fitting Linear Regression to the dataset
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
@@ -33,13 +23,6 @@ poly_reg.fit(X_poly, y)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
-# Visualising the Linear Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Linear Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
 
 # Visualising the Polynomial Regression results
 plt.scatter(X, y, color = 'red')
